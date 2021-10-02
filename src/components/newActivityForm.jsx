bdc9182e81307fbb64faadfca68e8524be31f013
vetal0352 <vetal0352@gmail.com>
2021-10-02 10:05:24 +0300

    2021-10-02 adding form elements

--- src/components/newActivityForm.jsx
+++ src/components/newActivityForm.jsx
@@ -1,5 +1,18 @@
 const NewActivityForm = () => {
-    return <div className="add-activity">Add new activity [Form]:</div>
+    // to do receiving activity types from DAL
+    const typesArray = ["ride", "run"]
+    return <div className="add-activity">
+        <form>
+            <b>Add new activity:</b>
+            <input name="startTime" type="time" placeholder="Start time HH:MM" />
+            <input name="finishTime" type="time" placeholder="Finish time HH:MM" />
+            <input name="distance" type={Number} placeholder="Distance" />
+            <select name="activityType" placeholder="Select activity type">
+                {typesArray.map(x => <option> {x} </option>)}
+            </select>
+            <button name="save">Save</button>
+        </form>
+    </div>
 }
 
 export default NewActivityForm
